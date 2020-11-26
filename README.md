e81d90a7dd6f2eb9bd45e3d6bfda4a6145242bd0
Germain Salvato Vallverdu <germain.vallverdu@gmail.com>
2020-11-26 23:50:50 +0100

    upload files

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # dash-example-NBA
+
 A web app hosted on heroku as an example to learn Dash
+
+Ce dossier contient un exemple basique d'une application Dash. Le code
+de l'application est dans le fichier `app.py`, le fichier `nba_physiques.csv`
+contient les données.
+
+Le jupyter notebook `NBA.ipynb` est un exemple de travail à réaliser en amont, 
+avant de faire l'application, pour explorer les données et construire les
+représentations graphiques.
+
+## Modules python nécessaires
+
+Pour installer Dash consulter la page relative à l'installation :
+https://dash.plotly.com/installation
+
+Pour une installation basique, depuis une ligne de commande, faire simplement :
+
+    pip install dash
+
+Pour les utilisateurs avancés, je vous conseilles de créer un environement 
+dédié à l'application. Vous pouvez le créer à partir du fichier 
+`environment.yml` contenu dans ce dossier, en l'important dans anaconda-navigator
+ou depuis la ligne de commande :
+
+    conda env create -f environment.yml
+
+
+## Lancer l'application
+
+Pour lancer l'application, depuis un terminal, exécuter la commande suivante :
+
+    > python app.py
+    Dash is running on http://127.0.0.1:8050/
+
+    * Serving Flask app "app" (lazy loading)
+    * Environment: production
+    WARNING: This is a development server. Do not use it in a production deployment.
+    Use a production WSGI server instead.
+    * Debug mode: on
+
+
+Dans votre navigateur, charger ensuite l'url http://127.0.0.1:8050/ (ou celle
+qui s'affichera).
+
+
+## contenu de l'application
+
+L'application est divisée en deux parties :
+* un graphique
+* un tableau
+
+Les deux menus déroulant au dessus du graphique contrôlent les variables portées
+en abscisse et en ordonnées.
+
+Le menu déroulant au dessus du tableau contrôle les valeurs qui sont regroupées
+dans le tableau.
+
+Ces deux actions sont réalisées grâce à deux fonctions (les `callback`). Voici
+un schéma de ces callbacks.
+
+![](callbacks.png)
+
+La sélection des objets est réalisée grâce aux identifiants des éléments de la
+page. Ils sont définis par les attributs `id` dans le code.
---
+++ app.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import dash
+from dash.dependencies import Input, Output
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+
+import pandas as pd
+
+# read data file
+# ------------------------------------------------------------------------------
+df = pd.read_csv(
+    "nba_physiques.csv",
+    index_col=0,
+    dtype={"Year": "int"}
+)
+# manage data
+df = df.assign(bmi=df.weight / ((df.height / 100) ** 2))
+df = df.assign(ht_bins=pd.qcut(df.height, q=4))
+df = df[["Year", "height", "weight", "bmi",
+         "PER", "PTS", "pos_simple", "ht_bins"]]
+
+
+# Set up app
+# ------------------------------------------------------------------------------
+external_stylesheets = [
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+# HTML page Layout
+# ------------------------------------------------------------------------------
+# Page is divided in three parts:
+#    * header: at the top, a title
+#    * body: the main containt
+#    * footer: at the bottom, contact, informations, credits
+app.layout = html.Div(className="", children=[
+    # ------ header
+    html.Div(
+        className="header",
+        style={"backgroundColor": "#3c6382"},
+        children=[html.H2(
+            "TITLE",
+            style={
+                "color": "white",
+                "padding": "30px 0 30px 0",
+                "textAlign": "center"}
+        )],
+    ),
+
+    # ----- body
+    html.Div(className="body", children=[
+        # a sub title
+        html.H3("A plot"),
+        # first dropdown selector
+        dcc.Dropdown(
+            id="x-dropdown",  # identifiant
+            value="height",  # default value
+            # all values in the menu
+            options=[{"label": name, "value": name} for name in df.columns],
+        ),
+        # second dropdown selector
+        dcc.Dropdown(
+            id="y-dropdown",
+            value="weight",
+            options=[{"label": name, "value": name} for name in df.columns],
+        ),
+        # a place for the plot with an id
+        html.Div(
+            dcc.Graph(id='graph'),
+        ),
+        # a line
+        html.Hr(),
+        # a sub title
+        html.H3("A table"),
+        # a new dropdown
+        dcc.Dropdown(
+            id="pivot-dropdown",
+            value="height",
+            options=[{"label": name, "value": name} for name in df.columns],
+        ),
+        # a table for data
+        dash_table.DataTable(
+            id="pivot-table",
+        ),
+        html.Div(id="table")
+    ]),
+
+    # ----- footer
+    html.Div(
+        className="footer",
+        style={"backgroundColor": "#3c6382"},
+        children=[html.H2(
+            "FOOTER",
+            style={
+                "color": "white",
+                "padding": "30px 0 30px 0",
+                "textAlign": "center"}
+        )],
+    ),
+])
+
+# Callback functions => interactivity
+# Each element on the page is identified thanks to its `id`
+# ------------------------------------------------------------------------------
+
+
+@app.callback(
+    Output('graph', 'figure'),
+    [Input("x-dropdown", "value"),
+     Input("y-dropdown", "value")],
+)
+def display_graph(xvalue, yvalue):
+    """ 
+    This function produce the plot.
+
+    The output is the "figure" of the graph
+    The inputs, are the values of the two dropdown menus
+    """
+
+    figure = px.scatter(
+        df,
+        x=xvalue, y=yvalue,
+        color='pos_simple',
+        category_orders=dict(pos_simple=['PG', 'SG', 'SF', 'PF', 'C']),
+        marginal_x="histogram",
+        marginal_y="histogram",
+        animation_group="Year",
+        template="plotly_white",
+    )
+
+    return figure
+
+
+@app.callback(
+    [Output("pivot-table", "data"),
+     Output("pivot-table", "columns")],
+    [Input("pivot-dropdown", "value")]
+)
+def show_pivot_table(value):
+    """ This function return a pivot Table """
+
+    pivot_df = pd.pivot_table(
+        data=df, values=value, columns="pos_simple", index="ht_bins"
+    )
+    pivot_df = pivot_df.reset_index()
+    pivot_df = pivot_df.astype({"ht_bins": "str"})
+    cols = [{"name": col, "id": col} for col in pivot_df.columns]
+    data = pivot_df.to_dict("records")
+
+    return data, cols
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
